5205fbbea7949e8d67fa0df3419656fea43ff4eb
Martin Gausby <martin@gausby.dk>
2018-04-02 14:21:10 +0200

    Subscribe to a socket on a Transmitter
    This will allow us to send the socket to the processes that need to
    publish QoS0 messages to the broker. No need to pass the data to the
    transmitter for that.
    
    When the subscribing process terminate the transmitter will clean up
    its subscriber state; when the transmitter receive a new socket it
    will broadcast the socket to its subscribers.

--- lib/tortoise/connection/transmitter.ex
+++ lib/tortoise/connection/transmitter.ex
@@ -5,7 +5,9 @@ defmodule Tortoise.Connection.Transmitter do
 
   alias Tortoise.Package
 
-  defstruct client_id: nil
+  defstruct client_id: nil, subscribers: %{}
+
+  @type client_id :: pid() | term()
 
   # Client API
   def start_link(opts) do
@@ -32,6 +34,21 @@ defmodule Tortoise.Connection.Transmitter do
     GenStateMachine.call(via_name(client_id), {:handle_socket, socket})
   end
 
+  def subscribe(client_id) do
+    GenStateMachine.call(via_name(client_id), :subscribe)
+  end
+
+  def unsubscribe(client_id) do
+    pid = self()
+    GenStateMachine.cast(via_name(client_id), {:unsubscribe, pid})
+  end
+
+  @doc false
+  @spec subscribers(client_id()) :: [pid()]
+  def subscribers(client_id) do
+    GenStateMachine.call(via_name(client_id), :get_subscribers)
+  end
+
   def ping(client_id) do
     pingreq = Package.encode(%Package.Pingreq{})
     GenStateMachine.cast(via_name(client_id), {:transmit, pingreq})
@@ -47,10 +64,23 @@ defmodule Tortoise.Connection.Transmitter do
     {:ok, :disconnected, data, []}
   end
 
-  def handle_event({:call, from}, {:handle_socket, socket}, :disconnected, data) do
+  def handle_event({:call, from}, {:handle_socket, socket}, _, data) do
     new_state = {:connected, socket}
-    next_actions = [{:reply, from, :ok}]
+
+    next_actions = [
+      {:reply, from, :ok},
+      {:next_event, :internal, :broadcast_connection_to_subscribers}
+    ]
+
     {:next_state, new_state, data, next_actions}
+  end
+
+  def handle_event(:internal, :broadcast_connection_to_subscribers, {:connected, socket}, data) do
+    for {subscriber, _} <- data.subscribers do
+      send_transmitter_to_subscriber(socket, subscriber)
+    end
+
+    :keep_state_and_data
   end
 
   def handle_event(:cast, {:transmit, package}, {:connected, socket}, _data) do
@@ -63,5 +93,47 @@ defmodule Tortoise.Connection.Transmitter do
   def handle_event(:cast, {:transmit, _package}, :disconnected, _data) do
     # postpone the data transmit for when we are online again
     {:keep_state_and_data, :postpone}
+  end
+
+  def handle_event({:call, {pid, _} = from}, :subscribe, _, %__MODULE__{} = data) do
+    monitor_ref = Process.monitor(pid)
+    next_actions = [{:reply, from, :ok}, {:next_event, :internal, {:send_socket, pid}}]
+    updated_subscribers = Map.put(data.subscribers, pid, monitor_ref)
+    updated_data = %__MODULE__{data | subscribers: updated_subscribers}
+    {:keep_state, updated_data, next_actions}
+  end
+
+  def handle_event(:internal, {:send_socket, subscriber}, {:connected, socket}, _data) do
+    send_transmitter_to_subscriber(socket, subscriber)
+    :keep_state_and_data
+  end
+
+  def handle_event(:internal, {:send_socket, _subscriber}, :disconnected, _data) do
+    # a connection will get broadcast when we get online
+    :keep_state_and_data
+  end
+
+  def handle_event(:cast, {:unsubscribe, pid}, _, %__MODULE__{} = data) do
+    {monitor_ref, updated_subscribers} = Map.pop(data.subscribers, pid)
+    updated_data = %__MODULE__{data | subscribers: updated_subscribers}
+    true = Process.demonitor(monitor_ref)
+    {:keep_state, updated_data}
+  end
+
+  def handle_event({:call, from}, :get_subscribers, _, %__MODULE__{} = data) do
+    subscribers = Map.keys(data.subscribers)
+    next_action = {:reply, from, subscribers}
+    {:keep_state_and_data, next_action}
+  end
+
+  def handle_event(:info, {:DOWN, ref, :process, pid, _}, _, data) do
+    {^ref, updated_subscribers} = Map.pop(data.subscribers, pid)
+    updated_data = %{data | subscribers: updated_subscribers}
+    {:keep_state, updated_data}
+  end
+
+  defp send_transmitter_to_subscriber(socket, subscriber_pid) do
+    transmitter = socket
+    send(subscriber_pid, {Tortoise, {:transmitter, transmitter}})
   end
 end
--- test/tortoise/connection/transmitter_test.exs
+++ test/tortoise/connection/transmitter_test.exs
@@ -10,11 +10,26 @@ defmodule Tortoise.Connection.TransmitterTest do
 
   def setup_transmitter(context) do
     opts = [client_id: context.client_id]
-    {:ok, _} = Transmitter.start_link(opts)
-    {:ok, client_socket, server_socket} = Tortoise.TestTCPTunnel.new()
-    Transmitter.handle_socket(context.test, client_socket)
+    {:ok, pid} = Transmitter.start_link(opts)
+    {:ok, %{transmitter_pid: pid}}
+  end
 
+  def setup_connection(context) do
+    {:ok, client_socket, server_socket} = Tortoise.TestTCPTunnel.new()
+    :ok = Transmitter.handle_socket(context.test, client_socket)
     {:ok, %{client: client_socket, server: server_socket}}
+  end
+
+  # update the context during a test run
+  def run_setup(context, setup) when is_atom(setup) do
+    context_update =
+      case apply(__MODULE__, setup, [context]) do
+        {:ok, update} -> update
+        [{_, _} | _] = update -> update
+        %{} = update -> update
+      end
+
+    Enum.into(context_update, context)
   end
 
   test "life-cycle", context do
@@ -22,5 +37,82 @@ defmodule Tortoise.Connection.TransmitterTest do
     assert Process.alive?(pid)
     assert :ok = Transmitter.stop(pid)
     refute Process.alive?(pid)
+  end
+
+  describe "subscribe/1" do
+    setup [:setup_transmitter]
+
+    test "subscribe while offline", context do
+      client_id = context.client_id
+      assert :ok = Transmitter.subscribe(client_id)
+      assert Transmitter.subscribers(client_id) == [self()]
+      # when we get a connection the subscribers should get a socket
+      _context = run_setup(context, :setup_connection)
+      assert_receive {Tortoise, {:transmitter, socket}}
+      assert is_port(socket)
+    end
+
+    test "subscribe while online", context do
+      client_id = context.client_id
+      _context = run_setup(context, :setup_connection)
+      assert :ok = Transmitter.subscribe(client_id)
+      assert Transmitter.subscribers(client_id) == [self()]
+      # The Transmitter should send the socket to the subscriber
+      assert_receive {Tortoise, {:transmitter, socket}}
+      assert is_port(socket)
+    end
+
+    test "remove subscription when the subscriber terminates", context do
+      client_id = context.client_id
+      # First create a subscription, for this test we will terminate
+      # the subscriber, so we need it to run in another process. To be
+      # able to control the timing we will need to send messages
+      # between the test process and the process that own the
+      # subscription; so we will use receive/1 and send/2 in this test
+      # for that purpose.
+      parent = self()
+      subscriber =
+        spawn_link(fn ->
+          :ok = Transmitter.subscribe(client_id)
+          # tell the parent that it can assert on the subscription
+          send(parent, {:subscribed, self()})
+
+          # keep the process alive until the parent tell it to release
+          receive do
+            :release ->
+              send(parent, {:released, self()})
+              :ok
+              # The subscriber will now terminate and the transmitter
+              # should remove it from its subscriber-list
+          end
+        end)
+
+      assert_receive {:subscribed, ^subscriber}
+      assert Transmitter.subscribers(client_id) == [subscriber]
+      send(subscriber, :release)
+      assert_receive {:released, ^subscriber}
+      assert Transmitter.subscribers(client_id) == []
+    end
+  end
+
+  describe "unsubscribe/1" do
+    setup [:setup_transmitter]
+
+    test "unsubscribe", context do
+      client_id = context.client_id
+      # first we create the subscription
+      assert :ok = Transmitter.subscribe(client_id)
+      context = run_setup(context, :setup_connection)
+      assert_receive {Tortoise, {:transmitter, _socket}}
+      # attempt to unsubscribe from the transmitter
+      :ok = Transmitter.unsubscribe(client_id)
+      assert Transmitter.subscribers(client_id) == []
+      # Refresh the socket which should trigger broadcast of the
+      # connection to the subscribers. We should no longer be
+      # subscribed at this point, so we should not receive the
+      # broadcast.
+      _context = run_setup(context, :setup_connection)
+      refute_receive {Tortoise, {:transmitter, _socket}}
+    end
   end
 end
