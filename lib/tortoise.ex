59cb5f2ee8d09d1b766d5a2522bd97ce7e622f23
Martin Gausby <martin@gausby.dk>
2018-05-14 23:02:23 +0200

    Initial documentation for the `publish/4` function

--- lib/tortoise.ex
+++ lib/tortoise.ex
@@ -4,7 +4,25 @@ defmodule Tortoise do
   """
 
   @doc """
-  Publish a message to the MQTT broker
+  Publish a message to the MQTT broker.
+
+  The publish function takes require a `client_id` and a valid MQTT
+  topic. If no `payload` is set an empty zero byte message will get
+  send to the broker.
+
+  Optionally an options list is accepted, which allow the user to
+  configure the publish as described in the following section.
+
+  ## Options
+
+    * `retain` indicates, when set to `true`, that the broker should
+      retain the message for the topic. Retained messages are
+      delivered to client when they subscribe to the topic. Only one
+      message can be retained for a given topic, so sending a new one
+      will overwrite the old. `retain` defaults to `false`.
+
+    * `qos` set the quality of service. The `qos` defaults to `0`.
+
   """
   defdelegate publish(client_id, topic, payload \\ nil, opts \\ []), to: Tortoise.Connection
 
