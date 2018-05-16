bbfa562582edb03c96c8053b77854f36507a532a
Martin Gausby <martin@gausby.dk>
2018-05-16 21:19:47 +0200

    Initial documentation of the public facing interface
    This is a start. It should get significantly improved with examples. I
    also miss a proper documentation for the module.

--- lib/tortoise.ex
+++ lib/tortoise.ex
@@ -26,14 +26,41 @@ defmodule Tortoise do
   """
   defdelegate publish(client_id, topic, payload \\ nil, opts \\ []), to: Tortoise.Connection
 
+  @doc """
+  Will publish a message to the broker. This is very similar to
+  `Tortoise.publish/4`, it takes the same configuration options, but
+  it will block the calling process until the message has been
+  delivered.
+
+      msg = "Hello, from the World of Tomorrow !"
+      case Tortoise.publish_sync("my_client_id", "foo/bar", msg) do
+        :ok ->
+          :done
+
+        {:error, :timeout} ->
+          :timeout
+      end
+
+  See the documentation for `Tortoise.publish/4` for configuration.
+  """
   defdelegate publish_sync(client_id, topic, payload \\ nil, opts \\ [], timeout \\ :infinity),
     to: Tortoise.Connection
 
+  @doc """
+  Subscribe to one or more topics using topic filters on `client_id`
+
+  The topic filter should be a 2-tuple, `{topic_filter, qos}`, where
+  the `topic_filter` is a valid MQTT topic filter, and `qos` an
+  integer value 0 through 2.
+
+  Multiple topics can be given as a list.
+  """
   defdelegate subscribe(client_id, topics, opts \\ []), to: Tortoise.Connection
 
+  @doc """
+  Unsubscribe from one of more topic filters. The topic filters are
+  given as strings. Multiple topic filters can be given at once by
+  passing in a list of strings.
+  """
   defdelegate unsubscribe(client_id, topics, opts \\ []), to: Tortoise.Connection
-
-  # defp generate_client_id() do
-  #   :crypto.strong_rand_bytes(10) |> Base.encode16()
-  # end
 end
