2bb3ac890dffc085393a41bf8c5326775986c3ad
Troels Brødsgaard <tr@arbr.dk>
2018-06-08 21:23:54 +0200

    Fix spec error in Tortoise.Registry

--- lib/tortoise/registry.ex
+++ lib/tortoise/registry.ex
@@ -4,7 +4,7 @@ defmodule Tortoise.Registry do
   @type client_id :: pid() | binary()
   @type via :: {:via, Registry, {__MODULE__, {atom(), client_id()}}}
 
-  @spec via_name(atom(), client_id()) :: via()
+  @spec via_name(atom(), client_id()) :: via() | pid()
   def via_name(_module, client_id) when is_pid(client_id), do: client_id
 
   def via_name(module, client_id) do
