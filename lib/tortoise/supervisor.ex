7d8b9391ec6e3f92b000ec69134cbe81dcbed5ff
Martin Gausby <martin@gausby.dk>
2018-06-20 22:57:25 +0200

    WIP documentation of the Tortoise.Supervisor module and functions

--- lib/tortoise/supervisor.ex
+++ lib/tortoise/supervisor.ex
@@ -1,5 +1,34 @@
 defmodule Tortoise.Supervisor do
-  @moduledoc false
+  @moduledoc """
+  A dynamic supervisor that can hold `Tortoise.Connection` processes
+
+  A `Tortoise.Supervisor`, registered under the name
+  `Tortoise.Supervisor`, is started as part of the `Tortoise.App`,
+  which will get started automatically when `Tortoise` is included as
+  a `Mix` dependency. Spawning a connection on the
+  `Tortoise.Supervisor` can be done as follows:
+
+      {:ok, pid} =
+        Tortoise.Supervisor.start_child(
+          client_id: "my_client_id",
+          handler: {Tortoise.Handler.Logger, []},
+          server: {Tortoise.Transport.Tcp, host: 'localhost', port: 1883},
+          subscriptions: [{"foo/bar", 0}]
+        )
+
+  While this is an easy way to get started one should consider the
+  supervision strategy of the application using the
+  `Tortoise.Connection`s. Often one would like to close the connection
+  with the application using the connection, in which case one could
+  consider starting the `Tortoise.Connection` directly in the
+  supervisor of the application using the connection, or start a
+  `Tortoise.Supervisor` as part of ones application if it require more
+  than one connection.
+
+  See the Connection Supervision article in the project documentation
+  for more information on connection supervision.
+
+  """
 
   use DynamicSupervisor
 
@@ -12,11 +41,24 @@ defmodule Tortoise.Supervisor do
     DynamicSupervisor.child_spec(opts)
   end
 
+  @doc """
+  Start a connection as a child of the `Tortoise.Supervisor`.
+
+  *Todo, rephrase this.*
+
+  The parent of the connection will be the `Tortoise.Supervisor` so
+  the process that call the `start_child/2` function will not get
+  linked to the connection; the supervisor will. One need to monitor
+  the connection, or use the `Tortoise.Connection.start_link/2`
+  directly, or attach the `Tortoise.Connection` to a supervision tree
+  if the connection should live with other parts of the system.
+  """
   def start_child(name \\ __MODULE__, opts) do
     spec = {Tortoise.Connection, opts}
     DynamicSupervisor.start_child(name, spec)
   end
 
+  @impl true
   def init(opts) do
     DynamicSupervisor.init(
       strategy: :one_for_one,
