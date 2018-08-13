eec9e28c37711a301ec9b567b89d08fc081f7071
Martin Gausby <martin@gausby.dk>
2018-08-13 22:19:10 +0200

    Test the QoS=2 publishes when dropping and reestablishing connection

--- test/tortoise/connection/inflight_test.exs
+++ test/tortoise/connection/inflight_test.exs
@@ -15,7 +15,7 @@ defmodule Tortoise.Connection.InflightTest do
     key = Tortoise.Registry.via_name(Tortoise.Connection, context.client_id)
     Tortoise.Registry.put_meta(key, connection)
     Tortoise.Events.dispatch(context.client_id, :connection, connection)
-    {:ok, %{client: client_socket, server: server_socket}}
+    {:ok, Map.merge(context, %{client: client_socket, server: server_socket})}
   end
 
   defp drop_connection(%{server: server} = context) do
@@ -69,6 +69,62 @@ defmodule Tortoise.Connection.InflightTest do
       Inflight.update(client_id, {:received, %Package.Puback{identifier: 1}})
 
       # the calling process should get a result response
+      assert_receive {{Tortoise, ^client_id}, ^ref, :ok}
+    end
+  end
+
+  describe "Publish with QoS=2" do
+    setup [:setup_connection, :setup_inflight]
+
+    test "incoming publish QoS=2", %{client_id: client_id} = context do
+      publish = %Package.Publish{identifier: 1, topic: "foo", qos: 2}
+      :ok = Inflight.track(client_id, {:incoming, publish})
+      assert {:ok, pubrec} = :gen_tcp.recv(context.server, 0, 500)
+      assert %Package.Pubrec{identifier: 1} = Package.decode(pubrec)
+
+      # drop and reestablish the connection
+      {:ok, context} = drop_connection(context)
+      {:ok, context} = setup_connection(context)
+
+      # now we should receive the same pubrec message
+      assert {:ok, ^pubrec} = :gen_tcp.recv(context.server, 0, 500)
+
+      # simulate that we receive a pubrel from the server
+      Inflight.update(client_id, {:received, %Package.Pubrel{identifier: 1}})
+
+      assert {:ok, pubcomp} = :gen_tcp.recv(context.server, 0, 500)
+      assert %Package.Pubcomp{identifier: 1} = Package.decode(pubcomp)
+    end
+
+    test "outgoing publish QoS=2", %{client_id: client_id} = context do
+      publish = %Package.Publish{identifier: 1, topic: "foo", qos: 2}
+      {:ok, ref} = Inflight.track(client_id, {:outgoing, publish})
+
+      # we should transmit the publish
+      assert {:ok, package} = :gen_tcp.recv(context.server, 0, 500)
+      assert ^publish = Package.decode(package)
+      # drop and reestablish the connection
+      {:ok, context} = drop_connection(context)
+      {:ok, context} = setup_connection(context)
+      # the publish should get re-transmitted
+      publish = %Package.Publish{publish | dup: true}
+      assert {:ok, package} = :gen_tcp.recv(context.server, 0, 500)
+      assert ^publish = Package.decode(package)
+
+      # simulate that we receive a pubrel from the server
+      Inflight.update(client_id, {:received, %Package.Pubrec{identifier: 1}})
+
+      # we should send the pubrel package
+      assert {:ok, pubrel} = :gen_tcp.recv(context.server, 0, 500)
+      assert %Package.Pubrel{identifier: 1} = Package.decode(pubrel)
+      # drop and reestablish the connection
+      {:ok, context} = drop_connection(context)
+      {:ok, context} = setup_connection(context)
+      # re-transmit the pubrel
+      assert {:ok, ^pubrel} = :gen_tcp.recv(context.server, 0, 500)
+
+      # When we receive the pubcomp message we should respond the caller
+      Inflight.update(client_id, {:received, %Package.Pubcomp{identifier: 1}})
       assert_receive {{Tortoise, ^client_id}, ^ref, :ok}
     end
   end
