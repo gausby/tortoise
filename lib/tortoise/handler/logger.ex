14bde6c19a51effbe34d5ead687a8cba8c9e8e9d
Martin Gausby <martin@gausby.dk>
2019-05-26 15:40:28 +0100

    Update the Tortoise.Handler.Logger to follow the new behaviour

--- lib/tortoise/handler/logger.ex
+++ lib/tortoise/handler/logger.ex
@@ -3,16 +3,20 @@ defmodule Tortoise.Handler.Logger do
 
   require Logger
 
+  alias Tortoise.Package
+
   use Tortoise.Handler
 
   defstruct []
   alias __MODULE__, as: State
 
+  @impl true
   def init(_opts) do
     Logger.info("Initializing handler")
     {:ok, %State{}}
   end
 
+  @impl true
   def connection(:up, state) do
     Logger.info("Connection has been established")
     {:cont, state}
@@ -28,37 +32,76 @@ defmodule Tortoise.Handler.Logger do
     {:cont, state}
   end
 
-  def subscription(:up, topic, state) do
-    Logger.info("Subscribed to #{topic}")
-    {:ok, state}
+  @impl true
+  def handle_connack(%Package.Connack{reason: :success}, state) do
+    Logger.info("Successfully connected to the server")
+    {:cont, state}
   end
 
-  def subscription({:warn, [requested: req, accepted: qos]}, topic, state) do
-    Logger.warn("Subscribed to #{topic}; requested #{req} but got accepted with QoS #{qos}")
-    {:ok, state}
-  end
-
-  def subscription({:error, reason}, topic, state) do
-    Logger.error("Error subscribing to #{topic}; #{inspect(reason)}")
-    {:ok, state}
-  end
-
-  def subscription(:down, topic, state) do
-    Logger.info("Unsubscribed from #{topic}")
-    {:ok, state}
-  end
-
-  def handle_message(topic, publish, state) do
-    Logger.info("#{Enum.join(topic, "/")} #{inspect(publish)}")
-    {:ok, state}
+  def handle_connack(%Package.Connack{reason: {:refused, refusal}}, state) do
+    Logger.error("Server refused connection: #{inspect refusal}")
+    {:cont, state}
   end
 
   @impl true
-  def handle_disconnect(disconnect, state) do
-    Logger.info("Received disconnect from server #{inspect(disconnect)}")
-    {:ok, state}
+  def handle_suback(%Package.Subscribe{} = subscribe, %Package.Suback{} = suback, state) do
+    for {{topic, opts}, result} <- Enum.zip(subscribe.topics, suback.acks) do
+      case {opts[:qos], result} do
+        {qos, {:ok, qos}} ->
+          Logger.info("Subscribed to #{topic} with the expected qos: #{qos}")
+
+        {req_qos, {:ok, accepted_qos}} ->
+          Logger.warn("Subscribed to #{topic} with QoS #{req_qos} but got accepted as #{accepted_qos}")
+
+        {_, {:error, reason}} ->
+          Logger.error("Failed to subscribe to topic: #{topic} reason: #{inspect reason}")
+      end
+    end
+    {:cont, state}
   end
 
+  @impl true
+  def handle_unsuback(%Package.Unsubscribe{} = unsubscribe, %Package.Unsuback{} = unsuback, state) do
+    for {topic, result} <- Enum.zip(unsubscribe.topics, unsuback.results) do
+      case result do
+        :success ->
+          Logger.info("Successfully unsubscribed from #{topic}")
+      end
+    end
+    {:cont, state}
+  end
+
+  @impl true
+  def handle_publish(topic, %Package.Publish{} = publish, state) do
+    Logger.info("#{Enum.join(topic, "/")} #{inspect(publish)}")
+    {:cont, state}
+  end
+
+  @impl true
+  def handle_puback(%Package.Puback{} = puback, state) do
+    Logger.info("Puback: #{puback.identifier}")
+    {:cont, state}
+  end
+
+  @impl true
+  def handle_pubrec(%Package.Pubrec{} = pubrec, state) do
+    Logger.info("Pubrec: #{pubrec.identifier}")
+    {:cont, state}
+  end
+
+  @impl true
+  def handle_pubcomp(%Package.Pubcomp{} = pubcomp, state) do
+    Logger.info("Pubcomp: #{pubcomp.identifier}")
+    {:cont, state}
+  end
+
+  @impl true
+  def handle_disconnect(%Package.Disconnect{} = disconnect, state) do
+    Logger.info("Received disconnect from server #{inspect(disconnect)}")
+    {:cont, state}
+  end
+
+  @impl true
   def terminate(reason, _state) do
     Logger.warn("Client has been terminated with reason: #{inspect(reason)}")
     :ok
