2ade95db7042e902a267458bdfe171809c34977e
Martin Gausby <martin.gausby@erlang-solutions.com>
2020-05-11 22:06:19 +0100

    Add a StreamData generator for Pingreq packages
    Check encoding and decoding as well. This is kind of silly, but done
    for completeness and it will make it easy for future improvements. It
    will always generate the same data as there are no fields on a pingreq
    struct besides the meta.

--- test/tortoise/package/pingreq_test.exs
+++ test/tortoise/package/pingreq_test.exs
@@ -1,15 +1,26 @@
 defmodule Tortoise.Package.PingreqTest do
   use ExUnit.Case
+  use ExUnitProperties
+
   doctest Tortoise.Package.Pingreq
 
   alias Tortoise.Package
 
-  test "encoding and decoding ping requests" do
-    pingreq = %Package.Pingreq{}
+  property "encoding and decoding pingreq messages" do
+    # as pingreqs always look the same it might be overkill to have a
+    # property based testing for this, but it is added for
+    # completeness; also in case of future changes to the protocol it
+    # might be eaiser to expand on this test, as I am hoping for user
+    # defined properties on ping request and responses (just kidding)
+    #
+    # Yeah, this is kind of silly...
+    config = %Package.Pingreq{}
 
-    assert ^pingreq =
-             pingreq
-             |> Package.encode()
-             |> Package.decode()
+    check all pingreq <- Package.generate(config) do
+      assert pingreq ==
+               pingreq
+               |> Package.encode()
+               |> Package.decode()
+    end
   end
 end
