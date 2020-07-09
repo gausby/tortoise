1d96fc310e655bee376d5ed7c7ee84d16624f56b
Martin Gausby <martin@gausby.dk>
2020-07-09 21:39:28 +0100

    Remove some cruft from the nix shell file

--- shell.nix
+++ shell.nix
@@ -5,19 +5,11 @@ with pkgs;
 let
   inherit (lib) optional optionals;
 
-  erlang_wx = erlangR21.override {
-    wxSupport = true;
-  };
-
-  elixir = (beam.packagesWith erlangR21).elixir.override {
-    version = "1.8.2";
-      rev = "98485daab0a9f3ac2d7809d38f5e57cd73cb22ac";
-      sha256 = "1n77cpcl2b773gmj3m9s24akvj9gph9byqbmj2pvlsmby4aqwckq";
-  };
+  elixir = (beam.packagesWith erlangR22).elixir;
 in
 
 mkShell {
-  buildInputs = [ elixir git wxmac ]
+  buildInputs = [ elixir ]
     ++ optional stdenv.isLinux inotify-tools # For file_system on Linux.
     ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
       # For file_system on macOS.
